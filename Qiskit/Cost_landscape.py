f9ec8ff39f9d9e954bd510efe2064e4f5df71957
seba2390 <75695915+seba2390@users.noreply.github.com>
2022-04-14 01:37:51 +0200

    Add files via upload

--- Qiskit/Cost_landscape.py
+++ Qiskit/Cost_landscape.py
@@ -14,25 +14,25 @@ from Qiskit_class import QiskitSimulation
 
 
 def cost_eval(my_sim, theta):
-    qc = my_sim.set_current_circuit(my_sim.initial_MSA, theta, normalize_weights=False)
+    qc = my_sim.set_current_circuit(my_sim.initial_MSA, theta)
     my_sim.current_counts = my_sim.current_backend.run(qc,seed_simulator=10).result().get_counts()
-    return my_sim.compute_expectation(my_sim.initial_MSA, my_sim.current_counts, normalize_weights = False) 
+    return my_sim.compute_expectation(my_sim.initial_MSA, my_sim.current_counts) 
 
 
 my_strings   = np.array(["AG","G"])
 my_penalties = np.array([1,1,1])*2
-my_msa       = MultipleSequenceAlignment(strings = my_strings, penalties = my_penalties) 
+my_msa       = MultipleSequenceAlignment(strings = my_strings, penalties = my_penalties, normalize_weights=True) 
 
 p = 2
 theta_0 = np.ones(p)
-my_simulation = QiskitSimulation(initial_MSA=my_msa, theta_0=theta_0, backend_name="qasm_simulator", normalize_weights=False, shots=10000)
+my_simulation = QiskitSimulation(initial_MSA=my_msa, theta_0=theta_0, backend_name="qasm_simulator", shots=10000)
 
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"},figsize=(10,10))
 
 # Make data.
-X = np.linspace(0, 2*np.pi, 100)
-Y = np.linspace(0, 2*np.pi, 100)
+X = np.linspace(0, 4*np.pi, 300)
+Y = np.linspace(0, 4*np.pi, 300)
 X, Y = np.meshgrid(X, Y)
 final_Z = []
 for col in tqdm(range(Y.shape[1])):
